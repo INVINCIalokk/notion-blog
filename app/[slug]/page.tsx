053ec510c1cd55434d4cc00a9a74f2156f18d0ed
alok_choudhary <alokc8502@gmail.com>
2024-05-05 16:42:23 +0530

    firt pull request for pwa

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -2,7 +2,7 @@ import { NotionPage } from "@/components/NotionRender";
 import { getNotionPage } from "@/components/NotionData";
 import { rootNotionPageId } from "@/lib/config";
 import BackButton from "@/components/button";
-
+import HomeButtion from '@/components/HomeButton'
 
 export default async function Page({ params }: { params: { slug: string } }) {
   let data;
@@ -14,10 +14,11 @@ export default async function Page({ params }: { params: { slug: string } }) {
   }
   return (
     <div className=''>
-      <div className="fixed bg-blue-700 w-full top-0 z-50 p-4">
+      <div className="fixed flex gap-2 bg-blue-700 w-full top-0 z-50 p-4">
+        <HomeButtion/>
         <BackButton/>
       </div>
-      <div className="mt-6">
+      <div className="mt-6 pt-2">
         <NotionPage recordMap={data.props.recordMap} rootPageId={rootNotionPageId} />
       </div>
     </div>
